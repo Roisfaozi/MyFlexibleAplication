7e9da26eb0a3859c30b624bf21b9b1758b987ec4
Rois Faozi <roisfaozi55@gmail.com>
2020-07-05 16:50:45 +0700

    Menambahkan Fragment Baru dan menambahkan fragment dialog

--- app/src/main/java/com/kotlin/myflexibleaplication/DetailCategoryFragment.kt
+++ app/src/main/java/com/kotlin/myflexibleaplication/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.kotlin.myflexibleaplication
 
+import android.graphics.Path
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_detail_category.*
 
 
@@ -63,9 +65,19 @@ class DetailCategoryFragment : Fragment(), View.OnClickListener {
 
             }
             R.id.btn_show_dialog -> {
+                val mOptionDialogFragment = OptionDialogFragment()
+
+                val mFragmentManager = childFragmentManager
+                mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment::class.java.simpleName)
 
             }
 
+        }
+    }
+
+    internal var optionDialogListener: OptionDialogFragment.OnOptionDialogListener = object : OptionDialogFragment.OnOptionDialogListener {
+        override fun onOptionChoosen(text: String?) {
+            Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
         }
     }
 }
